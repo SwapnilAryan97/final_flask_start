76b074619780dba71f58bbf4b7496d4b053dd891
swapnilaryan97 <saiamthere@gmail.com>
2022-05-11 19:13:56 -0400

    added user menu & auth test and formatted docker-compose.yml

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -1,11 +1,13 @@
 """This test the homepage"""
 
+
 def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
     assert b'href="/login"' in response.data
     assert b'href="/register"' in response.data
+
 
 def test_auth_pages(client):
     """This makes the index page"""
@@ -15,3 +17,25 @@ def test_auth_pages(client):
     assert response.status_code == 200
     response = client.get("/login")
     assert response.status_code == 200
+
+
+def test_user_menu_links(client):
+    """This tests the user links without login"""
+    response = client.get("/profile")
+    assert response.status_code == 200
+    response = client.get("/account")
+    assert response.status_code == 200
+    response = client.get("/logout")
+    assert response.status_code == 302
+    response = client.get("/users")
+    assert response.status_code == 302
+
+
+def test_auth_pages(client):
+    """This tests the home page"""
+    response = client.get("/login")
+    assert response.status_code == 200
+    response = client.get("/register")
+    assert response.status_code == 200
+    response = client.get("/dashboard")
+    assert response.status_code == 302
