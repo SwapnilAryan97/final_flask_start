89aa6ca0fbcb54a04cf9fcb2fe358959bd476523
swapnilaryan97 <saiamthere@gmail.com>
2022-05-11 19:34:33 -0400

    test for denial of dashboard access

--- tests/user_test.py
+++ tests/user_test.py
@@ -65,6 +65,7 @@ def test_login(client):
         assert login_response.request.path == url_for('auth.login')
         assert login_response.status_code == 400
 
+
 def test_dashboard_access(client):
     """This tests for successful access to dashboard after login"""
     with client:
@@ -79,5 +80,27 @@ def test_dashboard_access(client):
             "email": "test@test.com",
             "password": "testtest"
         }, follow_redirects=True)
+        assert login_response.request.path == url_for('auth.login')
+        assert login_response.status_code == 400
+
+
+def test_dashboard_access_denied(client):
+    """This tests for unsuccessful access to dashboard after login"""
+    with client:
+        register_response = client.post("/register", data={
+            "email": "test@test.com",
+            "password": "testtest",
+            "confirm": "testtest"
+        },
+                                        follow_redirects=True)
+
+        assert register_response.status_code == 400
+        assert register_response.request.path == url_for('auth.register')
+
+        login_response = client.post("/login", data={
+            "email": "test@test.com",
+            "password": "WRONG-PASSWORD"
+        },
+                                     follow_redirects=True)
         assert login_response.request.path == url_for('auth.login')
         assert login_response.status_code == 400
