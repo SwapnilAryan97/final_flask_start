7dce36b57f922cf576b020af3e810238c0908064
swapnilaryan97 <saiamthere@gmail.com>
2022-05-10 23:13:08 -0400

    added base files

--- tests/dummy_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-
-def test_request_index(client):
-    """This makes the first page"""
-    response = client.get("/")
-    assert response.status_code == 200
-    assert b"Hello, World!" in response.data
-
