c2af5b9a63d48450e3aed9cf9e7dc52c4b235411
swapnilaryan97 <saiamthere@gmail.com>
2022-05-11 19:30:18 -0400

    update configuration test

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,15 +7,16 @@ import pytest
 from app import create_app, User
 from app.db import db
 
-#this is a good tutorial I used to fix this code to do datbase testing.
-#https://xvrdm.github.io/2017/07/03/testing-flask-sqlalchemy-database-with-pytest/
+
+# this is a good tutorial I used to fix this code to do datbase testing.
+# https://xvrdm.github.io/2017/07/03/testing-flask-sqlalchemy-database-with-pytest/
 
 @pytest.fixture()
 def application():
     """This makes the app"""
-    #you need this one if you want to see whats in the database
-    #os.environ['FLASK_ENV'] = 'development'
-    #you need to run it in testing to pass on github
+    # you need this one if you want to see whats in the database
+    # os.environ['FLASK_ENV'] = 'development'
+    # you need to run it in testing to pass on github
     os.environ['FLASK_ENV'] = 'testing'
 
     application = create_app()
@@ -24,18 +25,17 @@ def application():
         db.create_all()
         yield application
         db.session.remove()
-        #drops the database tables after the test runs
-        #db.drop_all()
+        # drops the database tables after the test runs
+        # db.drop_all()
+
 
 @pytest.fixture()
 def add_user(application):
     with application.app_context():
-        #new record
+        # new record
         user = User('keith@webizly.com', 'testtest')
         db.session.add(user)
         db.session.commit()
-
-
 
 
 @pytest.fixture()
