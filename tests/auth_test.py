c263a9d71ffa2b5edb1914e229ccb789d48398b4
swapnilaryan97 <saiamthere@gmail.com>
2022-05-11 19:18:00 -0400

    test for successful login

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -20,7 +20,7 @@ def test_auth_pages(client):
 
 
 def test_user_menu_links(client):
-    """This tests the user links without login"""
+    """This tests the user links WITHOUT login"""
     response = client.get("/profile")
     assert response.status_code == 200
     response = client.get("/account")
@@ -31,11 +31,26 @@ def test_user_menu_links(client):
     assert response.status_code == 302
 
 
-def test_auth_pages(client):
-    """This tests the home page"""
-    response = client.get("/login")
-    assert response.status_code == 200
-    response = client.get("/register")
-    assert response.status_code == 200
-    response = client.get("/dashboard")
-    assert response.status_code == 302
+def test_user_menu_links_after_login(client):
+    with client:
+        """This tests the user menu links AFTER login"""
+        register_response = client.post("/register", data={
+            "email": "test@test.com",
+            "password": "testtest",
+            "confirm": "testtest"
+        },
+                                        follow_redirects=True)
+        login_response = client.post("/login", data={
+            "email": "test@test.com",
+            "password": "testtest"
+        },
+                                     follow_redirects=True)
+
+        assert login_response.status_code == 400
+
+        response = client.get("/profile")
+        assert response.status_code == 200
+        response = client.get("/account")
+        assert response.status_code == 200
+        response = client.get("/logout")
+        assert response.status_code == 302
