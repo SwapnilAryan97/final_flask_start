d7c89f15423f772b59d07ade59e9eb555f23eed6
swapnilaryan97 <saiamthere@gmail.com>
2022-05-11 19:34:02 -0400

    test for dashboard access

--- tests/user_test.py
+++ tests/user_test.py
@@ -64,3 +64,20 @@ def test_login(client):
         # After successful login ,redirected to dashboard
         assert login_response.request.path == url_for('auth.login')
         assert login_response.status_code == 400
+
+def test_dashboard_access(client):
+    """This tests for successful access to dashboard after login"""
+    with client:
+        register_response = client.post("/register", data={
+            "email": "test@test.com",
+            "password": "testtest",
+            "confirm": "testtest"
+        },
+                                        follow_redirects=True)
+
+        login_response = client.post("/login", data={
+            "email": "test@test.com",
+            "password": "testtest"
+        }, follow_redirects=True)
+        assert login_response.request.path == url_for('auth.login')
+        assert login_response.status_code == 400
