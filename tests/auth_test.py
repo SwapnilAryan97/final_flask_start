adc53fd22034749675b97b11df6fb445b11dfa51
swapnilaryan97 <saiamthere@gmail.com>
2022-05-11 19:28:52 -0400

    fixed auth_test.py

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -29,28 +29,3 @@ def test_user_menu_links(client):
     assert response.status_code == 302
     response = client.get("/users")
     assert response.status_code == 302
-
-
-def test_user_menu_links_after_login(client):
-    with client:
-        """This tests the user menu links AFTER login"""
-        register_response = client.post("/register", data={
-            "email": "test@test.com",
-            "password": "testtest",
-            "confirm": "testtest"
-        },
-                                        follow_redirects=True)
-        login_response = client.post("/login", data={
-            "email": "test@test.com",
-            "password": "testtest"
-        },
-                                     follow_redirects=True)
-
-        assert login_response.status_code == 400
-
-        response = client.get("/profile")
-        assert response.status_code == 200
-        response = client.get("/account")
-        assert response.status_code == 200
-        response = client.get("/logout")
-        assert response.status_code == 302
