2beb88c9e22504317788d1108d40e9ab72635e6d
swapnilaryan97 <saiamthere@gmail.com>
2022-05-11 22:29:45 -0400

    login test

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -28,3 +28,21 @@ def test_auth_pages(client):
     assert response.status_code == 200
 
 
+def test_login(application, client):
+    """Test that a user login functionality works"""
+    with application.app_context():
+        # Add user to be able to test login
+        user = User('vt7@njit.edu', 'testtest')
+        db.session.add(user)
+        db.session.commit()
+
+        res = client.post('/login', data=dict(email="vt7@njit.edu", password =generate_password_hash('testtest')), follow_redirects=True)
+        assert res.status_code == 400
+        # assert b"Welcome" in res.data
+
+        # Test that the user can navigate to the dashboard and that it displays the user
+        dres = client.get("/dashboard", follow_redirects=True)
+
+        db.session.delete(user)
+
+
