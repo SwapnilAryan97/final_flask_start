aecdf8863c6596c4fcfdff77767ec85b005dab63
swapnilaryan97 <saiamthere@gmail.com>
2022-05-11 22:31:18 -0400

    user authentication test

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -5,7 +5,7 @@ from app.db.models import User
 from flask import Blueprint, render_template, abort, url_for, current_app
 from flask_login import current_user, login_required
 from jinja2 import TemplateNotFound
-from flask import Blueprint, render_template, redirect, url_for, flash,current_app
+from flask import Blueprint, render_template, redirect, url_for, flash, current_app
 from flask_login import login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash
 
@@ -36,7 +36,8 @@ def test_login(application, client):
         db.session.add(user)
         db.session.commit()
 
-        res = client.post('/login', data=dict(email="vt7@njit.edu", password =generate_password_hash('testtest')), follow_redirects=True)
+        res = client.post('/login', data=dict(email="vt7@njit.edu", password=generate_password_hash('testtest')),
+                          follow_redirects=True)
         assert res.status_code == 400
         # assert b"Welcome" in res.data
 
@@ -63,6 +64,7 @@ def test_registration(application, client):
         assert user is None
         assert response.status_code == 400
 
+
 def test_dashboard_access(client):
     """Test dashboard access"""
     res = client.get("/dashboard")
@@ -73,3 +75,12 @@ def test_upload_access(client):
     res = client.get("/transactions/upload")
     assert res.status_code == 302
 
+
+def test_authenticated_user(client, application, add_user):
+    """Test user authentication"""
+    with application.app_context():
+        user = User("vt7@njit.edu", "testtest")
+        db.session.add(user)
+        assert user.is_authenticated() == True
+        res = client.post('/login', data=dict(email="vt7@njit.edu", password='testtest'), follow_redirects=True)
+        assert user.is_active() == True
