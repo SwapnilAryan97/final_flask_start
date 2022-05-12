7b398442f1edcce52d69c52730697d026800086d
swapnilaryan97 <saiamthere@gmail.com>
2022-05-11 22:40:09 -0400

    added context variable year

--- tests/context_process_test.py
+++ tests/context_process_test.py
@@ -10,3 +10,14 @@ def test_context_variables_environment(client):
     assert response.status_code == 200
     assert content in response.data
 
+def test_context_variables_year(client):
+    """This tests checks if the copyright and current year are printed"""
+    response = client.get("/")
+    current_date_time = datetime.datetime.now()
+    date = current_date_time.date()
+    year = date.strftime("%Y")
+    test_string = f"Copyright: {year}"
+    content = bytes(test_string, 'utf-8')
+    assert response.status_code == 200
+    assert content in response.data
+
