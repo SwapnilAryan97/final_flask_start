8540881d05b06a973ebd7c45a3a00df2f8600012
swapnilaryan97 <saiamthere@gmail.com>
2022-05-11 22:44:09 -0400

    added index test

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -10,3 +10,9 @@ def test_request_main_menu_links(client):
     assert b'href="/register"' in response.data
 
 
+def test_request_index(client):
+    """This makes the index page"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Index" in response.data
+
