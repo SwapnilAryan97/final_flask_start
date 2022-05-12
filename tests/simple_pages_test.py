b9c3f726376d711e60862d88dc2accd0a512d49e
swapnilaryan97 <saiamthere@gmail.com>
2022-05-11 22:44:25 -0400

    added about page test

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -16,3 +16,9 @@ def test_request_index(client):
     assert response.status_code == 200
     assert b"Index" in response.data
 
+def test_request_about(client):
+    """This makes the index page"""
+    response = client.get("/about")
+    assert response.status_code == 200
+    assert b"About" in response.data
+
