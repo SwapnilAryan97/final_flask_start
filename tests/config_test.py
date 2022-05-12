11eacdb51158c1ca0a17c51d372608c007676c77
swapnilaryan97 <saiamthere@gmail.com>
2022-05-11 22:37:19 -0400

    added testing config

--- tests/config_test.py
+++ tests/config_test.py
@@ -11,4 +11,11 @@ def test_development_config(application):
     assert application.config['DEBUG']
     assert not application.config['TESTING']
 
+def test_testing_config(application):
+    application.config.from_object('app.config.TestingConfig')
+    assert application.config['DEBUG']
+    assert application.config['TESTING']
+    assert not application.config['PRESERVE_CONTEXT_ON_EXCEPTION']
+    assert application.config['SQLALCHEMY_DATABASE_URI'] == 'sqlite:///:memory:'
+
 
