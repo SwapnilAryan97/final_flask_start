760360a0d175d709a2a8a6951eae7d48818c98c4
swapnilaryan97 <saiamthere@gmail.com>
2022-05-11 22:43:51 -0400

    added main menu test

---
+++ tests/simple_pages_test.py
@@ -0,0 +1,12 @@
+"""This test the homepage"""
+
+def test_request_main_menu_links(client):
+    """This makes the index page"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b'href="/about"' in response.data
+    assert b'href="/welcome"' in response.data
+    assert b'href="/login"' in response.data
+    assert b'href="/register"' in response.data
+
+
