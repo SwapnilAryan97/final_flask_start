1ad73061bb5eecfe0163d7a9d6ee430c4923de5e
swapnilaryan97 <saiamthere@gmail.com>
2022-05-11 22:46:44 -0400

    adding user test

--- tests/user_test.py
+++ tests/user_test.py
@@ -4,3 +4,14 @@ from app import db
 from app.db.models import User, Transaction
 from faker import Faker
 from sqlalchemy.sql import functions
+
+
+def test_adding_user(application):
+    log = logging.getLogger("myApp")
+    with application.app_context():
+        assert db.session.query(User).count() == 0
+        assert db.session.query(Transaction).count() == 0
+        user = User('tnvrra393@gmail.com', 'spiderCLAW')
+        db.session.add(user)
+        db.session.commit()
+        assert db.session.query(User).count() == 1
